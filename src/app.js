d1600e2f9947a7db7d6c0ccad78713ab7fc070ec
Cleyton de Castro <cleytoncm@gmail.com>
2020-10-18 21:23:29 -0300

    Adciona ediçao, exclusao e acrescimo de likes

--- src/app.js
+++ src/app.js
@@ -20,6 +20,7 @@ app.post("/repositories", (request, response) => {
   const repository = {
     id: uuid(),
     title,
+    url,
     techs,
     likes: 0
   };
@@ -30,15 +31,56 @@ app.post("/repositories", (request, response) => {
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json({ error: 'Project not fount' });
+  }
+
+  if (title !== '') {
+    repositories[repositoryIndex].title = title;
+  }
+
+  if (url !== '') {
+    repositories[repositoryIndex].url = url;
+  }
+
+  if (typeof techs !== 'undefined' && typeof techs !== 'array') {
+    repositories[repositoryIndex].techs = techs;
+  }
+  
+  return response.json(repositories[repositoryIndex]);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json({ error: 'Project not fount' });
+  }
+
+  repositories.slice(repositoryIndex, 1);
+
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+
+  if (repositoryIndex < 0) {
+    return response.status(400).json({ error: 'Project not fount' });
+  }
+
+  repositories[repositoryIndex].likes++;
+
+  return response.json(repositories[repositoryIndex].likes);
 });
 
 module.exports = app;
